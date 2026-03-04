17726060b2bd161ed9aaa34c59a639d2ec4ecf07
ShunKUN1182 <2250378@ecc.ac.jp>
2026-03-04 15:05:38 +0900

    feat: ストレージデータを更新

--- js/result.js
+++ js/result.js
@@ -1,5 +1,10 @@
 const modal = document.querySelector(".modal");
 const btns = document.querySelectorAll(".data_wrap");
+const codeName = document.querySelector("#codeName");
+const rankName = document.querySelector("#rankName");
+const getPoint = document.querySelector("#getPoint");
+
+codeName.textContent = localStorage.getItem("codeName");
 
 btns.forEach((e) => {
     e.addEventListener("click", () => {
